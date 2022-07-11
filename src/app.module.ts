dd74964695bc6d7294db155a6fe087ce38436481
ShahirZain <hshahir@Macs-MacBook-Pro.local>
2022-07-11 22:07:40 +0500

    setuped prisma to the software

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,6 @@
 import { Module } from '@nestjs/common';
+import { PrismaModule } from './prisma/prisma.module';
 @Module({
-  imports: [],
+  imports: [PrismaModule],
 })
 export class AppModule {}
---
+++ src/prisma/prisma.service.ts
@@ -0,0 +1,15 @@
+import { Injectable, OnModuleDestroy, OnModuleInit } from '@nestjs/common';
+import { PrismaClient } from '@prisma/client';
+
+@Injectable()
+export class PrismaService
+  extends PrismaClient
+  implements OnModuleInit, OnModuleDestroy
+{
+  async onModuleInit() {
+    await this.$connect();
+  }
+  onModuleDestroy() {
+    throw new Error('Method not implemented.');
+  }
+}
