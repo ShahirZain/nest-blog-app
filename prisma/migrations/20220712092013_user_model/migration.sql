12223e9b9322de91aa19ff58452ecd1a1044ae6c
ShahirZain <hshahir@Macs-MacBook-Pro.local>
2022-07-12 14:25:38 +0500

    sync DB with schema

---
+++ prisma/migrations/20220712092013_user_model/migration.sql
@@ -0,0 +1,16 @@
+-- RedefineTables
+PRAGMA foreign_keys=OFF;
+CREATE TABLE "new_User" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "name" TEXT,
+    "createAd" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updateAd" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+INSERT INTO "new_User" ("email", "id", "name", "password") SELECT "email", "id", "name", "password" FROM "User";
+DROP TABLE "User";
+ALTER TABLE "new_User" RENAME TO "User";
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+PRAGMA foreign_key_check;
+PRAGMA foreign_keys=ON;
